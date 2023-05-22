a51dcebcd6ea039fe777485a691338043dc248d1
Tah282 <128704260+Tah282@users.noreply.github.com>
2023-05-22 15:42:16 +0900

    [level 0] Title: 369게임, Time: 8.08 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String str = ""+order;
+        for(int i=0; i<str.length(); i++){
+            char num = str.charAt(i);
+            if( num=='3' || num=='6' || num=='9' )
+                answer += 1;
+        }
+        
+        
+        return answer;
+    }
+}
